1b6739f6df4d7f2923c33dbee78b814b4edfd5f2
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-07-27 20:19:10 +0200

    Initial Reactjs app setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <nav class="navbar navbar-light bg-light">
+              <div class="navbar-brand">Todo app</div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
